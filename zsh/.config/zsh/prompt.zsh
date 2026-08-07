a4b75f350eb3ca43914a948baffe3f0e917d05c4
Sarvagya Sharma <sarvagya2545@gmail.com>
2026-08-07 22:31:35 +0530

    fix: starship runtime sourcing issue

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -1,2 +1,9 @@
 export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
+
+# see link: https://github.com/starship/starship/issues/3418#issuecomment-2477375663
+if [[ "${widgets[zle-keymap-select]#user:}" == "starship_zle-keymap-select" || \
+      "${widgets[zle-keymap-select]#user:}" == "starship_zle-keymap-select-wrapped" ]]; then
+    zle -N zle-keymap-select "";
+fi
+
 eval "$(starship init zsh)"
