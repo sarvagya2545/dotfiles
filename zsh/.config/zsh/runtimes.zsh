b646846ad0a02963f0c48154e4a08a5435283313
Sarvagya Sharma <sarvagya2545@gmail.com>
2026-08-07 22:38:37 +0530

    fix: fnm runtime for node

--- zsh/.config/zsh/runtimes.zsh
+++ zsh/.config/zsh/runtimes.zsh
@@ -1,8 +1,8 @@
 
 # fnm
-FNM_PATH="/home/ubuntu/.local/share/fnm"
+FNM_PATH="$HOME/.local/share/fnm"
 if [ -d "$FNM_PATH" ]; then
-  export PATH="$FNM_PATH:$PATH"
-  eval "$(fnm env --shell zsh)"
+    export PATH="$FNM_PATH:$PATH"
+    eval "$(fnm env --use-on-cd --shell zsh)"
 fi
 
