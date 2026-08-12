c2d980db892b0494596b2610ebe16bfff78b51df
Sarvagya Sharma <sarvagya2545@gmail.com>
2026-08-12 05:21:41 +0000

    fix: zsh cursor icon

--- zsh/.config/zsh/bindings.zsh
+++ zsh/.config/zsh/bindings.zsh
@@ -2,15 +2,18 @@
 # Keybindings
 # =========================================================
 
-# Cursor shape per vi mode
-ZVM_INSERT_MODE_CURSOR=$ZVM_CURSOR_BEAM
-ZVM_NORMAL_MODE_CURSOR=$ZVM_CURSOR_BLOCK
-ZVM_VISUAL_MODE_CURSOR=$ZVM_CURSOR_BLOCK
+function zvm_config() {
+    # Cursor shape per vi mode
+    ZVM_INSERT_MODE_CURSOR=$ZVM_CURSOR_BLOCK
+    ZVM_NORMAL_MODE_CURSOR=$ZVM_CURSOR_BLOCK
+    ZVM_VISUAL_MODE_CURSOR=$ZVM_CURSOR_BLOCK
 
-# Disable command mode line highlight
-ZVM_VI_HIGHLIGHT_BACKGROUND=none
-ZVM_VI_HIGHLIGHT_FOREGROUND=none
-ZVM_VI_HIGHLIGHT_EXTRASTYLE=none
+    # Disable command mode line highlight
+    ZVM_VI_HIGHLIGHT_BACKGROUND=none
+    ZVM_VI_HIGHLIGHT_FOREGROUND=none
+    ZVM_VI_HIGHLIGHT_EXTRASTYLE=none
+}
+
 
 # zsh-vi-mode resets all bindings on init, so custom bindings
 # must be registered via this hook to survive.
